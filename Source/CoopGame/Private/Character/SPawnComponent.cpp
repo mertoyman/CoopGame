a844933cdaf85c9994a8289f13204671dea9ba0e
Mert Oyman <mertoyman@gmail.com>
2022-05-24 11:36:05 +0300

    Created Pawn Component class

---
+++ Source/CoopGame/Private/Character/SPawnComponent.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/SPawnComponent.h"
+
+USPawnComponent::USPawnComponent(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryComponentTick.bStartWithTickEnabled(false);
+	PrimaryComponentTick.bCanEverTick(false);
+}
---
+++ Source/CoopGame/Public/Character/SPawnComponent.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PawnComponent.h"
+#include "SPawnComponent.generated.h"
+
+UINTERFACE(BlueprintType)
+class COOPGAME_API USReadyInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ISReadyInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool IsPawnComponentReadyToInitialize() const = 0;
+};
+/**
+ * SPawnComponent
+ *
+ * An actor component that can be used for adding custom behavior to pawns.
+ */
+UCLASS(Blueprintable, meta=(BlueprintSpawnableComponent))
+class COOPGAME_API USPawnComponent : public UPawnComponent, public ISReadyInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	USPawnComponent(const FObjectInitializer& ObjectInitializer);
+
+	virtual bool IsPawnComponentReadyToInitialize() const override { return true; }
+};
